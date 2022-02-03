27e22f31095dbfe8203908b2b797fa3411d45614
ensfrknkc <ensfrknkc58@gmail.com>
2022-02-04 01:35:51 +0300

    ContextAdded

--- DataAccess/Concrete/EntityFramework/NortwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,5 +12,9 @@ namespace DataAccess.Concrete.EntityFramework
         {
             optionsBuilder.UseSqlServer(@"Server=ENES\SQLEXPRESS;Database=Northwind;Trusted_Connection=true");
         }
+
+        public DbSet<Product> Products  { get; set; }
+        public DbSet<Category> Categories  { get; set; }
+        public DbSet<Customer> Customers  { get; set; }
     }
 }
