6765db7e6830b178a5d2b493fcbaa3350b9defa4
ensfrknkc <ensfrknkc58@gmail.com>
2022-02-14 01:05:46 +0300

    ResultArchitectureAdded

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,16 +19,36 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public List<Product> GetAll()
+        public IResult Add(Product product)
         {
-            //Iş Kodları
-            //Yetkisi var mı?
-            return _productDal.GetAll();
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult<List<Product>>("Ürün Listeleme Başarısız");
+            }
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"Ürünler Listelendi");
         }
 
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(x => x.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
